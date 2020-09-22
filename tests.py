b721dbaf6d3a2fc4b552863b7624d08dc020ac36
scottymichaelh <scott.halasy@gmail.com>
2020-09-22 12:53:02 -0500

    Update unit tests and fix registration template bug

--- tests.py
+++ tests.py
@@ -5,12 +5,15 @@ from app.models import User, Post
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')
@@ -87,3 +90,7 @@ class UserModelCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
